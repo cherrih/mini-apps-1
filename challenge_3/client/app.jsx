15ec3aa77a6f0871c2da84ad554cf7756f472789
cherrih <cherri.hartigan@gmail.com>
2018-12-12 14:28:43 -0800

    Render UI using React, compile using babel

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,20 @@
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>welcome to your virtual shopping experience</h2>
+        <Checkout />
+      </div>
+    )
+  }
+}
+
+const Checkout = props => {
+  return (
+    <button>Checkout</button>
+  )
+}
 
 
+
+ReactDOM.render(<App/>, document.getElementById("app"));
